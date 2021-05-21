a197fde7bc6902c929e35c826b7ae6213af23cfe
ocedal <ocedal@DESKTOP-SCSGFM8.localdomain>
2021-05-21 14:01:14 -0700

    4

---
+++ al05/function.js
@@ -0,0 +1,21 @@
+function rotate(matrix) {
+  const side = matrix.length
+  for (let i = 0; i < side / 2; i++) {
+    for (let j = i; j < side - i - 1; j++) {
+      const firstVal = matrix[i][j]
+      matrix[i][j] = matrix[j][side - 1 - i]
+      matrix[j][side - 1 - i] = matrix[side - 1 - i][side - 1 - j]
+      matrix[side - 1 - i][side - 1 - j] = matrix[side - 1 - j][i]
+      matrix[side - 1 - j][i] = firstVal
+    }
+  }
+  return matrix
+}
+const mat = [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15],
+  [16, 17, 18, 19, 20],
+  [21, 22, 23, 24, 25],
+]
+console.log(rotate(mat))
